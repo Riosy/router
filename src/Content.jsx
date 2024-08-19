7ccfb6eda6ccdb999c74bba51179b2b7a140ef82
Riosy <riosycreates@gmail.com>
2024-08-19 19:15:53 +0330

    React router P1

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,6 +5,7 @@ import Posts from './posts/Posts';
 import style from './style.module.css'
 import Todos from './todos/Todos';
 import Users from './users/Users';
+import { Routes , Route} from "react-router-dom";
 
 const Content = ()=>{
 
@@ -13,18 +14,21 @@ const Content = ()=>{
     const handleShowMenu = (event)=>{
         event.stopPropagation()
         setShowMenu(!showMenu)
-        console.log(showMenu);
+   ;
     }
 
     return (
         <div className={style.content_section} onClick={()=>{setShowMenu(false)}}>
             <i className={`${style.menu_button} fas fa-bars text-dark m-2 pointer`} 
             onClick={handleShowMenu}
-            ></i>
-            <Users/>
-            <Posts/>
-            <Gallery/>
-            <Todos/>
+            ></i>        
+ <Routes>
+    <Route path='/'element={<Users/>} />
+    <Route path='/posts'element={<Posts/>} />
+    <Route path='/gallery'element={<Gallery/>} />
+    <Route path='/todos'element={<Todos/>} />
+ </Routes>
+
         </div>
     )
 
